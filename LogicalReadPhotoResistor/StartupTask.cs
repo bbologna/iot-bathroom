cd3433e841ebfa9eeec14bb7e5b805c6e8475512
Dan Pardo <dan@kzsoftworks.com>
2017-02-01 22:51:21 -0300

    Added iot hub client setup. Installed nuget packages. Add missed file.

--- LogicalReadPhotoResistor/StartupTask.cs
+++ LogicalReadPhotoResistor/StartupTask.cs
@@ -1,20 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Net.Http;
 using Windows.ApplicationModel.Background;
 using Windows.Devices.Gpio;
-using System.Threading;
-using Windows.System.Threading;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
 namespace LogicalReadPhotoResistor
 {
-    public sealed class StartupTask : IBackgroundTask
+	public sealed class StartupTask : IBackgroundTask
     {
-        BackgroundTaskDeferral _deferral;
+		static DeviceClient deviceClient;
+		static string iotHubUri = "bathroom-project.azure-devices.net";
+		static string deviceName = "bathroom_IoT";
+		static string deviceKey = "1gWPtWtBw2LmtcMnJfQmzSzaWvASfz96ISYp7ZD0Ud4=";
+
+		BackgroundTaskDeferral _deferral;
         private const int LED_PIN = 6;
         private const int RESISTOR_PIN = 5;
 
@@ -25,6 +27,7 @@ namespace LogicalReadPhotoResistor
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             _deferral = taskInstance.GetDeferral();
+			SetUpClientDevice();
             SetupLed();
             SetupPhotoresistor();
         }
@@ -47,12 +50,34 @@ namespace LogicalReadPhotoResistor
             resistorPin.ValueChanged += PhotoresistorChanged;
         }
 
+		private void SetUpClientDevice()
+		{
+			deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(deviceName, deviceKey), TransportType.Http1);
+		}
+
         private void PhotoresistorChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
         {
             var value = sender.Read();
             ledPin.Write(value);
             System.Diagnostics.Debug.WriteLine(value);
+
+			var status = value == GpioPinValue.Low ? "Not occupied" : "occupied";
+
+			SendDeviceToCloudMessagesAsync(status);
         }
 
-    }
+		private static async void SendDeviceToCloudMessagesAsync(string status)
+		{
+			var bathroomData = new
+			{
+				deviceId = deviceName,
+				time = DateTime.Now.ToString(),
+				roomStatus = status
+			};
+			var messageString = JsonConvert.SerializeObject(bathroomData);
+			var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+			await deviceClient.SendEventAsync(message);
+		}
+	}
 }
